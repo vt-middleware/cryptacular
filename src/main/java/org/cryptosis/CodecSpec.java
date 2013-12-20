8a2f8f08d197d76fafe495178fe28a34a71ec062
Marvin S. Addison <marvin.addison@gmail.com>
2013-12-20 13:20:35 -0500

    Convert CodecSpec to class for consistency.

--- src/main/java/org/cryptosis/CodecSpec.java
+++ src/main/java/org/cryptosis/CodecSpec.java
@@ -12,13 +12,40 @@ import org.cryptosis.codec.HexEncoder;
  *
  * @author Marvin S. Addison
  */
-public enum CodecSpec
+public class CodecSpec
 {
-  /** Hexadecimal encoding. */
-  Hex,
+  /** Hexadecimal encoding specification. */
+  public static CodecSpec HEX = new CodecSpec("Hex");
 
-  /** Base64 encoding. */
-  Base64;
+  /** Base64 encoding specification. */
+  public static CodecSpec BASE64 = new CodecSpec("Base64");
+
+
+  /** Name of encoding, e.g. "Hex, "Base64". */
+  private String encoding;
+
+
+  /**
+   * Creates a new instance of the given encoding.
+   *
+   * @param  encoding  Name of encoding.
+   */
+  public CodecSpec(final String encoding)
+  {
+    if (encoding == null) {
+      throw new IllegalArgumentException("Encoding cannot be null.");
+    }
+    this.encoding = encoding;
+  }
+
+
+  /**
+   * @return  The name of the encoding, e.g. "Hex", "Base64".
+   */
+  public String getEncoding()
+  {
+    return encoding;
+  }
 
 
   /**
@@ -27,9 +54,9 @@ public enum CodecSpec
   public Encoder newEncoder()
   {
     final Encoder encoder;
-    if ("Hex".equals(name())) {
+    if ("Hex".equalsIgnoreCase(encoding)) {
       encoder = new HexEncoder();
-    } else if ("Base64".equals(name())) {
+    } else if ("Base64".equalsIgnoreCase(encoding) || "Base-64".equalsIgnoreCase(encoding)) {
       encoder = new Base64Encoder();
     } else {
       throw new IllegalArgumentException("Invalid encoding.");
@@ -44,9 +71,9 @@ public enum CodecSpec
   public Decoder newDecoder()
   {
     final Decoder decoder;
-    if ("Hex".equals(name())) {
+    if ("Hex".equalsIgnoreCase(encoding)) {
       decoder = new HexDecoder();
-    } else if ("Base64".equals(name())) {
+    } else if ("Base64".equalsIgnoreCase(encoding) || "Base-64".equalsIgnoreCase(encoding)) {
       decoder = new Base64Decoder();
     } else {
       throw new IllegalArgumentException("Invalid encoding.");
