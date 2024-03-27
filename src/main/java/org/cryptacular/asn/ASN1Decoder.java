67364eeecb3e92c9ed07ee9a718808c910cad3e4
Erdem A Memisyazici <ememisya@vt.edu>
2024-03-27 13:20:50 -0400

    Support decoding SSH public keys.
    Provides support for both PEM and binary encodings per RFC 4253.
    
    Resolves #69

--- src/main/java/org/cryptacular/asn/ASN1Decoder.java
+++ src/main/java/org/cryptacular/asn/ASN1Decoder.java
@@ -1,7 +1,7 @@
 /* See LICENSE for licensing and NOTICE for copyright. */
 package org.cryptacular.asn;
 
-import org.cryptacular.EncodingException;
+import org.cryptacular.KeyDecoder;
 
 /**
  * Strategy interface for converting encoded ASN.1 bytes to an object.
@@ -10,18 +10,6 @@ import org.cryptacular.EncodingException;
  *
  * @author  Middleware Services
  */
-public interface ASN1Decoder<T>
+public interface ASN1Decoder<T> extends KeyDecoder<T>
 {
-
-  /**
-   * Produces an object from an encoded representation.
-   *
-   * @param  encoded  ASN.1 encoded data.
-   * @param  args  Additional data required to perform decoding.
-   *
-   * @return  Decoded object.
-   *
-   * @throws  EncodingException  on encoding errors.
-   */
-  T decode(byte[] encoded, Object... args) throws EncodingException;
 }
